11d0399f9918235e0acb4667f2138b85cc6cb009
Florent <grenaille.florent85@gmail.com>
2019-03-06 15:26:01 +0100

    Modification du script de base de données -> ajout d'une table

--- createDataBase.sql
+++ createDataBase.sql
@@ -2,20 +2,29 @@ CREATE DATABASE IF NOT EXISTS purbeurre;
 
 USE purbeurre;
 
+CREATE TABLE pb_categories
+(
+    id_categorie INT NOT NULL AUTO_INCREMENT UNSIGNED,
+    categorie_name VARCHAR(45) NOT NULL UNIQUE,
+    PRIMARY KEY(id_categorie)
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE TABLE pb_aliments
 (
-    id_aliments INT NOT NULL AUTO_INCREMENT,
-    aliment_categorie VARCHAR(45) NOT NULL UNIQUE,
+    id_aliments INT NOT NULL AUTO_INCREMENT UNSIGNED,
+    aliment_categorie_idCategorie VARCHAR(45) NOT NULL UNIQUE,
     aliment_name VARCHAR(45) NOT NULL UNIQUE,
     aliment_shop VARCHAR(45),
     aliment_link VARCHAR(120),
     PRIMARY KEY(id_aliments)
+    FOREIGN KEY(aliment_categorie_idCategorie) REFERENCES pb_categories(id_categorie)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE pb_favoris
 (
-    id_favoris INT NOT NULL AUTO_INCREMENT,
+    id_favoris INT NOT NULL AUTO_INCREMENT UNSIGNED,
     favoris_aliment_idAliments INT,
     PRIMARY KEY(id_favoris),
     FOREIGN KEY(favoris_aliment_idAliments) REFERENCES pb_aliments(id_aliments)
@@ -24,9 +33,9 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE pb_users
 (
-    id_users INT NOT NULL AUTO_INCREMENT,
+    id_users INT NOT NULL AUTO_INCREMENT UNSIGNED,
     user_name VARCHAR(45) NOT NULL UNIQUE,
-    user_passwd VARCHAR(65) NOT NULL,
+    user_passwd VARCHAR(88) NOT NULL,
     user_favoris_idFavoris INT,
     PRIMARY KEY(id_users),
     FOREIGN KEY(user_favoris_idFavoris) REFERENCES pb_favoris(id_favoris)
