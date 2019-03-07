ed8ed01ca11d26a5a9f3882a8f7380a870d81104
Florent <grenaille.florent85@gmail.com>
2019-03-07 11:56:16 +0100

    Modification du script de base de données SQL

--- createDataBase.sql
+++ createDataBase.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS purbeurre;
 
 USE purbeurre;
 
+------------------------------------------------------------------------------
 CREATE TABLE pb_categories
 (
     id_categorie INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -9,35 +10,48 @@ CREATE TABLE pb_categories
     PRIMARY KEY(id_categorie)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------
 CREATE TABLE pb_aliments
 (
     id_aliments INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    aliment_categorie_idCategorie VARCHAR(45) NOT NULL UNIQUE,
+    aliment_categorie VARCHAR(45) NOT NULL UNIQUE,
     aliment_name VARCHAR(45) NOT NULL UNIQUE,
     aliment_shop VARCHAR(45),
     aliment_link VARCHAR(120),
-    PRIMARY KEY(id_aliments),
-    FOREIGN KEY(aliment_categorie_idCategorie) REFERENCES pb_categories(id_categorie)
+    PRIMARY KEY(id_aliments)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------
 CREATE TABLE pb_favoris
 (
     id_favoris INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    favoris_aliment_idAliments INT,
-    PRIMARY KEY(id_favoris),
-    FOREIGN KEY(favoris_aliment_idAliments) REFERENCES pb_aliments(id_aliments)
+    favoris_aliment INT UNSIGNED,
+    PRIMARY KEY(id_favoris)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------
 CREATE TABLE pb_users
 (
     id_users INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_name VARCHAR(45) NOT NULL UNIQUE,
     user_passwd VARCHAR(88) NOT NULL,
-    user_favoris_idFavoris INT,
-    PRIMARY KEY(id_users),
-    FOREIGN KEY(user_favoris_idFavoris) REFERENCES pb_favoris(id_favoris)
+    user_favoris INT UNSIGNED,
+    PRIMARY KEY(id_users)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+------------------------------------------------------------------------------
+
+-------Clé étrangère----------------------------------------------------------
+ALTER TABLE pb_aliments
+ADD CONSTRAINT FK_pb_aliments FOREIGN KEY(aliment_categorie) REFERENCES pb_categories(categorie_name);
+ALTER TABLE pb_favoris
+ADD CONSTRAINT FK_pb_favoris FOREIGN KEY(favoris_aliment) REFERENCES pb_aliments(id_aliments);
+ALTER TABLE pb_users
+ADD CONSTRAINT FK_pb_users FOREIGN KEY(user_favoris) REFERENCES pb_favoris(id_favoris);
+------------------------------------------------------------------------------
