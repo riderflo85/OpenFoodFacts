85e3aa39cfb125ae8f9183bae4c2a79232ae98a1
Florent <grenaille.florent85@gmail.com>
2019-03-15 12:22:15 +0100

    Correction d'une erreur dans database.sql

--- ressources/database.sql
+++ ressources/database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS purbeurre;
 USE purbeurre;
 CREATE TABLE pb_categories (id_categorie INT UNSIGNED NOT NULL AUTO_INCREMENT, categorie_name VARCHAR(45) NOT NULL UNIQUE, PRIMARY KEY(id_categorie)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE TABLE pb_aliments (id_aliments INT UNSIGNED NOT NULL AUTO_INCREMENT, aliment_name VARCHAR(140) NOT NULL UNIQUE, aliment_categorie VARCHAR(45) NOT NULL, alimment_nutrition VARCHAR(2), aliment_nova_group VARCHAR(2), aliment_shop VARCHAR(45), aliment_link VARCHAR(140), PRIMARY KEY(id_aliments)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE TABLE pb_aliments (id_aliments INT UNSIGNED NOT NULL AUTO_INCREMENT, aliment_name VARCHAR(140) NOT NULL UNIQUE, aliment_categorie VARCHAR(45) NOT NULL, aliment_nutrition VARCHAR(2), aliment_nova_group VARCHAR(2), aliment_shop VARCHAR(45), aliment_link VARCHAR(140), PRIMARY KEY(id_aliments)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE pb_favoris (id_favoris INT UNSIGNED NOT NULL AUTO_INCREMENT, favoris_aliment INT UNSIGNED,PRIMARY KEY(id_favoris)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE pb_users (id_users INT UNSIGNED NOT NULL AUTO_INCREMENT, user_name VARCHAR(45) NOT NULL UNIQUE, user_passwd VARCHAR(88) NOT NULL, user_favoris INT UNSIGNED, PRIMARY KEY(id_users)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE pb_aliments ADD CONSTRAINT FK_pb_aliments FOREIGN KEY(aliment_categorie) REFERENCES pb_categories(categorie_name);
